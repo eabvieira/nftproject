73e815dc938d02b2209efa7c899f22ffdf7713c1
eabvieira <eabvieira370@gmai.com>
2022-10-10 14:43:33 -0300

    Código

---
+++ adminpanel/src/app/constants/config.ts
@@ -0,0 +1,6 @@
+export const config = {
+    site_name: "Cryptotrades",
+    media_path: "https://exampleapi.com",
+    base_url: "https://exampleadmin.com",
+    frontend_url: "https://example.com"
+}
